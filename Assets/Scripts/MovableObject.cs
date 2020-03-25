e01632b6ac5af79381b638a1af5d951fb869dc35
PolyAtomicIon <180107265@stu.sdu.edu.kz>
2020-03-25 23:21:32 +0600

    second

---
+++ Assets/Scripts/MovableObject.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovableObject : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+
+    private boolean pushable;
+
+    void Freeze_position(){
+        rb.constraints = RigidbodyConstraints2D.FreezePosition;
+    }
+
+    void reFreeze_position(){
+        rb.constraints = RigidbodyConstraints2D.None;
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        pushable = false;
+        
+        Freeze_position();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("collision enter " + collision.gameObject.tag );
+        if( collision.gameObject.tag == "Player"){
+            if( !pushable ){
+                reFreeze_position();
+            }   
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        Debug.Log("collision exit " + collision.gameObject.tag );
+        if( collision.gameObject.tag == "Player" && !pushable  ){
+            Freeze_position();
+        }
+    }
+
+}
---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+    public Camera cam;
+
+    Vector2 movement;
+    Vector3 current_velocity_vector;
+    Vector2 mousePos;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void FixedUpdate(){
+                
+        Vector2 direction = (movement.x * -transform.up + movement.y * transform.right).normalized;
+        rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2( lookDir.y, lookDir.x ) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+
+       // rb.MovePosition(rb.position + (Vector2) transform.forward * moveSpeed * Time.deltaTime);
+
+    }
+
+}
